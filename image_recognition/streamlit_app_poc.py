7213a7367e7c01bb7c7f27e9c789d1798f39687f
Kevin Kroell <kkroell01@gmail.com>
2022-11-14 18:09:43 +0100

    Add streamlit_app_poc.py
    Run with: streamlit run streamlit_app.py

---
+++ image_recognition/streamlit_app_poc.py
@@ -0,0 +1,44 @@
+import torch
+import os
+from PIL import Image
+from transformers import VisionEncoderDecoderModel, ViTFeatureExtractor, AutoTokenizer
+import streamlit as st
+
+st.title('Amazing AVPRG stuff happing :alien:')
+st.title("Upload Your Image")
+
+img = st.file_uploader('')
+
+model = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+feature_extractor = ViTFeatureExtractor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+
+max_length = 16
+num_beams = 4
+gen_kwargs = {"max_length": max_length, "num_beams": num_beams}
+
+
+def predict_single_img(img):
+    if img is not None:
+        i_image = Image.open(img)
+        if i_image.mode != "RGB":
+            i_image = i_image.convert(mode="RGB")
+
+        pixel_values = feature_extractor(images=i_image, return_tensors="pt").pixel_values
+        pixel_values = pixel_values.to(device)
+
+        output_ids = model.generate(pixel_values, **gen_kwargs)
+
+        preds = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
+        preds = [pred.strip() for pred in preds]
+        return preds
+
+if st.button('Predict'):
+    st.write(predict_single_img(img)[0])
+
+
+if __name__ == "__main__":
+    pass

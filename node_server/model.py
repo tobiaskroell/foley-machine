81d546ff4ab5bdaeb7cd9f640d0c4440331a9af8
Kevin Kroell <kkroell01@gmail.com>
2022-11-25 23:00:39 +0100

    Add POST route for /video

---
+++ node_server/model.py
@@ -0,0 +1,10 @@
+def detectAnimals():
+    """
+    Returns a dictionary of detected animals
+    """
+
+    return ({
+        "dog": "00:00:03", 
+        "cat": "00:00:05", 
+        "bird": "00:00:07"
+        })

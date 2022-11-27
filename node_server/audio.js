74a2c4762feed9cf403e8f442f200ed952833403
Mo Uhlig <mouhlig@gmail.com>
2022-11-27 17:11:32 +0100

    audio basics working

--- node_server/audio.js
+++ node_server/audio.js
@@ -1,43 +1,42 @@
-let context = new AudioContext();
+var context = new AudioContext();
 var audioBuffers = [];
+var bufferSourceNodes = [];
+var gainNodes = [];
+var audioPath = "audio/";
+var audioFormat = ".wav";
 
 audioFiles = ["sound1", "sound2", "sound3", "sound4"];
 
-
-function getAudioData(i) {
-  fetch("sound" + (i + 1)+".wav")
-      .then(response => response.arrayBuffer())
-      .then(undecodedAudio => context.decodeAudioData(undecodedAudio))
-      .then(audioBuffer => {
-          audioBuffers[i] = audioBuffer;
-  })
-  .catch(console.error);
+// function that loads audio buffer data in the audioBuffers array
+function getAudioData(fileName, i) {
+  fetch(fileName + audioFormat)
+    .then(response => response.arrayBuffer())
+    .then(undecodedAudio => context.decodeAudioData(undecodedAudio))
+    .then(audioBuffer => {
+      audioBuffers[i] = audioBuffer;
+    })
+    .catch(console.error);
 }
+// execute the function for each audio file
+for (let i = 0; i < audioFiles.length; i++)
+  getAudioData(audioFiles[i], i);
 
-for (let i = 0; i < 4; i++)
-    getAudioData(i);
-
-function playSound(buffer, time) {
-  let source = context.createBufferSource();
-  source.buffer = buffer;
-  source.connect(context.destination);
-  source.start(time);
-}
-
-function createAudioNodes(audioFiles) {
-  
-  for (let i = 0; i < audioFiles.length; i++) {
-    let gainNode = context.createGain();
-    let sound = new Audio(audioFiles[i]+".wav");
-    let source = context.createMediaElementSource(sound);
-    gainNode.gain.value = 0.5;
-    source.connect(gainNode);
-    gainNode.connect(context.destination);
+// connects an buffer from the audioBuffers array to a gain node and then to the destination, 
+// plays the sound at the given time
+function playSoundAtTime(i, time) {
+  bufferSourceNodes[i] = context.createBufferSource();
+  bufferSourceNodes[i].buffer = audioBuffers[i];
+  if (typeof gainNodes[i] == 'undefined') {
+      gainNodes[i] = context.createGain();
+      gainNodes[i].gain.value = 0.5;
   }
-
+  bufferSourceNodes[i].connect(gainNodes[i]);
+  gainNodes[i].connect(context.destination);
+  bufferSourceNodes[i].start(context.currentTime + time);
 }
 
-function createAudioDiv(audioFiles = []) {
+//create div elements with audio control elements attaches event listeners to sliders
+function createAudioDiv(audioFiles) {
   let parentDiv = document.getElementById("audioElementsFrame");
   let heading = document.createElement("h1")
   heading.textContent = "Audio Mixer"
@@ -49,30 +48,26 @@ function createAudioDiv(audioFiles = []) {
     channel.setAttribute("id", `channel${i}`)
     channel.innerHTML = returnAudioElement(audioFiles[i], i)
     parentDiv.appendChild(channel)
-    document.querySelector("#volumeSlider" + i).addEventListener("input", changeParameter);
+    document.querySelector("#volumeSlider"+i).addEventListener("input", function (e) {
+      changeParameter(e,i)
+    });
+  }
 }
-}
-function changeParameter() {
-  console.log(this.id, "volumeSlider")
-  switch (this.id) {
-    case "volumeSlider0":
-      document.querySelector("#volumeOutput0").innerHTML = (this.value) + " dB";
-      console.log('hello world')
+// event handler for all sliders
+function changeParameter(e,i) {
+  switch (e.target.id) {
+    case "volumeSlider"+i:
+      document.querySelector("#volumeOutput"+i).innerHTML = (e.target.value / 100) ;
+      gainNodes[i].gain.value = e.target.value / 100;
+      
       break;
     case "detuneSlider":
       filter.detune.value = (this.value);
       document.querySelector("#detuneOutput").innerHTML = (this.value) + " cents";
       break;
-    case "qSlider":
-      filter.Q.value = (this.value);
-      document.querySelector("#qOutput").innerHTML = (this.value) + " ";
-      break;
-    case "gainSlider":
-      filter.gain.value = (this.value);
-      document.querySelector("#gainOutput").innerHTML = (this.value) + " dB";
-      break;
   }
 }
+// function that returns html for audio control elements
 function returnAudioElement(name, channel) {
   return `
   <div>
@@ -81,68 +76,17 @@ function returnAudioElement(name, channel) {
     <div>
     <label for="volume">Volume</label>
     <input class="slider" type="range" id="volumeSlider${channel}" name="volume" min="0" max="100" value="50">
-    <span id="volumeOutput${channel}" class="output">- 10 dB</span>
+    <span id="volumeOutput${channel}" class="output"> 0.5 </span>
     </div>
 
   `
 }
-function playBeat() {
-  let tempo = 90; // BPM (beats per minute)
-  let eighthNoteTime = (60 / tempo) / 2;
-  let startTime = context.currentTime;
-  let bassdrum = audioBuffers[0];
-  let snaredrum = audioBuffers[1];
-  let hihat = audioBuffers[2];
-
-  for (let takt = 0; takt < 2; takt++) {
-      let time = startTime + (takt * 8 * eighthNoteTime);
-
-      playSound(bassdrum, time + 0 * eighthNoteTime);
-      playSound(bassdrum, time + 1 * eighthNoteTime);
-      playSound(bassdrum, time + 4 * eighthNoteTime);
-
-      playSound(snaredrum, time + 2 * eighthNoteTime);
-      playSound(snaredrum, time + 3.5 * eighthNoteTime);
-      playSound(snaredrum, time + 4.5 * eighthNoteTime);
-      playSound(snaredrum, time + 6 * eighthNoteTime);
-
-      for (let j = 0; j < 8; j++) {
-          playSound(hihat, time + j * eighthNoteTime);
-      }
-  }
-}
-document.querySelector("#playPauseButton").addEventListener("click", function(e) {
-  playBeat();
+document.querySelector("#playPauseButton").addEventListener("click", function (e) {
+  playSoundAtTime(0, 0);
+  playSoundAtTime(1, 1);
+  playSoundAtTime(2, 2);
+  playSoundAtTime(3, 3);
+  playSoundAtTime(0, 4);
 });
-
-
-
-createAudioNodes(audioFiles);    
+   
 createAudioDiv(audioFiles);
-
-  /*
-  var sliders = document.getElementsByClassName("slider");
-  for (var i = 0; i < sliders.length; i++)
-    sliders[i].addEventListener('mousemove', changeParameter(i), false);
-    console.log('hello world2')
-    */
-
-    /* for maybe later use
-
-      <div>
-    <label for="volume">Pitch</label>
-    <input class="slider" type="range" id="detuneOutput" name="volume" min="0" max="100" value="50">
-  </div>
-  <div>
-    <label for="volume">Pan</label>
-    <input class="slider" type="range" id="volume" name="volume" min="0" max="100" value="50">
-  </div>
-  <div>
-    <label for="volume">Speed</label>
-    <input class="slider" type="range" id="volume" name="volume" min="0" max="100" value="50">
-  </div>
-  <div>
-    <label for="volume">Distortion</label>
-    <input class="slider" type="range" id="volume" name="volume" min="0" max="100" value="50">
-  </div>
-  */

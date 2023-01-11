3a7a559478bc766bef6c9b13e98dfe889fca6c75
Leander Wernst <leanderwernst@msn.com>
2023-01-11 15:19:25 +0100

    set local python server address in node server script

--- node_server/server.js
+++ node_server/server.js
@@ -61,7 +61,7 @@ app.timeout = 120000; // 2 minutes (ms); default is 2 minutes
  ***********/
 const pyProduction = 'http://3.122.106.66';
 const pyLocal = 'http://127.0.0.1:8000';
-const pyServer = PORT == 3000 ? pyLocal : pyProduction;
+const pyServer = pyLocal;
 const pyPath = "/video"
 
 // Serving static files
